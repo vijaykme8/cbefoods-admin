0623746c4706d7568562820a26c40b8b600cb90f
Vijay <vijaykumar@Vijays-MacBook-Air.local>
2026-05-29 17:08:42 +0530

    Tone down admin color system

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cbe-admin-step4-visual-system';
+const CACHE='cbe-admin-step4-toned-colors';
 const ASSETS=['./','./index.html','./styles.css','./admin.js','./manifest.webmanifest','./icons/admin-icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
