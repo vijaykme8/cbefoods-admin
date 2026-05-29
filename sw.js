e2ee25ad07f52f231ec08209dfbb59f843e9243d
Vijay <vijaykumar@Vijays-MacBook-Air.local>
2026-05-29 19:12:42 +0530

    Redesign admin UI using calendar reference

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cbe-admin-reference-only-ui';
+const CACHE='cbe-admin-calendar-reference-ui';
 const ASSETS=['./','./index.html','./styles.css','./admin.js','./manifest.webmanifest','./icons/admin-icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
