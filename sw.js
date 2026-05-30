5b1e81b758d5fba6e88df706bed1b787310cbe3a
Vijay <vijaykumar@Vijays-MacBook-Air.local>
2026-05-30 12:31:30 +0530

    Use provided sidebar SVG icons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cbe-admin-all-features';
+const CACHE='cbe-admin-sidebar-exact-icons';
 const ASSETS=['./','./index.html','./styles.css','./admin.js','./manifest.webmanifest','./icons/admin-icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
