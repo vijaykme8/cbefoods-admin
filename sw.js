fb5d642cf0676e49ed801d74bc348f3ae77fae2a
Vijay <vijaykumar@Vijays-MacBook-Air.local>
2026-05-30 09:50:45 +0530

    Redesign sidebar with premium glass reference style

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cbe-admin-ui-cleanup';
+const CACHE='cbe-admin-sidebar-premium-ref';
 const ASSETS=['./','./index.html','./styles.css','./admin.js','./manifest.webmanifest','./icons/admin-icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
